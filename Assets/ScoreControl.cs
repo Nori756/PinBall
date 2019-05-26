80f77472bcb3a03c706153cac3feebee45f178f4
NORI756 <kiyohide826@gmail.com>
2019-05-26 21:29:36 +0900

    PinBallの課題、応用課題を処理したので確認お願いします。iphone 6Sの画面サイズで設定しました。

---
+++ Assets/ScoreControl.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreControl : MonoBehaviour {
+
+    public GameObject scoreText;
+    public int score = 0;
+    
+    
+    // Use this for initialization
+	void Start () {
+        this.scoreText = GameObject.Find("ScoreText");
+
+        score = 0;
+        SetScore();
+
+
+
+	}
+    void OnCollisionEnter(Collision collision)
+    {
+        string yourTag = collision.gameObject.tag;
+
+        if (yourTag == "SmallStarTag")
+        {
+            score += 10;
+        }
+        else if (yourTag == "LargeStarTag")
+        {
+            score += 100;
+        }
+        else if (yourTag == "SmallCloudTag" )
+        {
+            score += 50;
+        }
+        else if (yourTag ==  "LargeCloudTag")
+        {
+            score += 200;
+        }
+  
+        SetScore();
+    }
+
+    void SetScore()
+    {
+        this.scoreText.GetComponent<Text>().text= string.Format("Score:{0}", score);
+    }
+
+
+
+    // Update is called once per frame
+    void Update () {
+        
+	}
+}
---
+++ Assets/TouchController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour
+    {
+        //HingeJointコンポーネントを入れる
+        private HingeJoint myHingeJoint;
+
+        //初期の傾き
+        public float defaultAngle = 20;
+        //弾いた時の傾き
+        public float flickAngle = -20;
+
+        // Use this for initialization
+        void Start()
+        {
+            //HingeJointコンポーネント取得
+            this.myHingeJoint = GetComponent<HingeJoint>();
+
+            //フリッパーの傾きを設定
+            SetAngle(this.defaultAngle);
+        }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Touch myTouch = Input.GetTouch(0);
+
+        Touch[] myTouches = Input.touches;
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+
+
+
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+
+                TouchPhase phase = touch.phase;
+                switch (touch.phase)
+                {
+
+                    case TouchPhase.Began:
+                        //画面右半分をタッチしていたら右フリッパーが傾く
+                        if (touch.position.x > Screen.width * 0.5f && tag == "RightFripperTag")
+                        {
+                            SetAngle(this.flickAngle);
+                        }
+                        //画面左半分をタッチしていたら左フリッパーが傾く
+                        if (touch.position.x < Screen.width * 0.5f && tag == "LeftFripperTag") {
+                            SetAngle(this.flickAngle);
+                        }
+                        break;
+                     case TouchPhase.Ended:
+            //離された時フリッパーを元に戻す
+            if (touch.position.x > Screen.width * 0.5f && tag == "RightFripperTag")
+                        {
+                            SetAngle(this.defaultAngle);
+                        }
+                        //離された時フリッパーを元に戻す
+                        if (touch.position.x < Screen.width * 0.5f && tag == "LeftFripperTag")
+                        {
+                            SetAngle(this.defaultAngle);
+                        }
+                        break; }
+            }
+        }
+    }
+        //フリッパーの傾きを設定
+        public void SetAngle(float angle)
+        {
+            JointSpring jointSpr = this.myHingeJoint.spring;
+            jointSpr.targetPosition = angle;
+            this.myHingeJoint.spring = jointSpr;
+        }
+    }
+
+
+       
